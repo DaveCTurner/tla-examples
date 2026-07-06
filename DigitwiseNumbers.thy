5cc6f77cb50a797b32e7bc9f9501aee8ce5c0232
David Turner <david.turner@elastic.co>
2026-07-06 09:27:07 +0100

    WIP

--- DigitwiseNumbers.thy
+++ DigitwiseNumbers.thy
@@ -11,9 +11,7 @@ definition wellformed :: "nat list \<Rightarrow> bool"
   where "wellformed ds \<equiv> set ds \<subseteq> {0..9} \<and> (ds = [] \<or> last ds \<noteq> 0)"
 
 lemma wellformed_Nil[simp]: "wellformed []" by (simp add: wellformed_def)
-lemma wellformed_ConsI: "wellformed ds \<Longrightarrow> d \<le> 9 \<Longrightarrow> (ds = [] \<Longrightarrow> 0 < d) \<Longrightarrow> wellformed (d#ds)" by (auto simp add: wellformed_def)
-lemma wellformed_ConsE: "wellformed (d#ds) \<Longrightarrow> wellformed ds" by (auto simp add: wellformed_def, cases "ds = []", simp_all)
-
+lemma wellformed_eq[simp]: "wellformed (d#ds) \<longleftrightarrow> (wellformed ds \<and> d \<le> 9 \<and> (ds = [] \<longrightarrow> 0 < d))" by (auto simp add: wellformed_def)
 
 function (sequential) applyCarry1 :: "nat \<Rightarrow> nat list \<Rightarrow> nat list"
   where
@@ -49,15 +47,7 @@ lemma applyCarry1_last[simp]: "(applyCarry1 c ds = []) \<longleftrightarrow> (c 
   by (induct c ds rule: applyCarry1_induct, simp_all)
 
 lemma applyCarry1_wellformed: "wellformed ds \<Longrightarrow> wellformed (applyCarry1 c ds)"
-proof (induct c ds rule: applyCarry1_induct)
-  case (Cons c d ds)
-  thus ?case
-    unfolding Cons.hyps(3) using wellformed_ConsE
-  proof (intro wellformed_ConsI Cons.hyps(2))
-    assume "applyCarry1 ((c + d) div 10) ds = []" "0 < c"
-    thus "0 < (c + d) mod 10" by auto
-  qed simp_all
-qed (auto simp add: wellformed_def)
+  by (induct c ds rule: applyCarry1_induct, auto)
 
 fun applyCarry2 :: "nat \<Rightarrow> nat list \<Rightarrow> nat list \<Rightarrow> nat list"
   where
@@ -94,14 +84,5 @@ lemma applyCarry2_wellformed:
   assumes wf1: "wellformed ds1"
   assumes wf2: "wellformed ds2"
   shows "wellformed (applyCarry2 c ds1 ds2)"
-  using applyCarry1_wellformed [OF wf1] applyCarry1_wellformed [OF wf2] assms
-proof (induct c ds1 ds2 rule: applyCarry2_induct)
-  case (Cons c d1 ds1 d2 ds2)
-  from `wellformed (d1 # ds1)` `wellformed (d2 # ds2)` have wf12: "wellformed ds1" "wellformed ds2" by (metis wellformed_ConsE)+
-  show ?case unfolding Cons
-  proof (intro wellformed_ConsI Cons applyCarry1_wellformed wf12)
-    assume "applyCarry2 ((c + d1 + d2) div 10) ds1 ds2 = []" hence p: "(c + d1 + d2) div 10 = 0" "ds1 = []" "ds2 = []" by simp_all
-    with `wellformed (d1 # ds1)` `wellformed (d2 # ds2)`
-    show "0 < (c + d1 + d2) mod 10" by (auto simp add: wellformed_def)
-  qed simp
-qed simp_all
+  using applyCarry1_wellformed [OF wf1] applyCarry1_wellformed [OF wf2] assms applyCarry1_wellformed
+  by (induct c ds1 ds2 rule: applyCarry2_induct, force+)
